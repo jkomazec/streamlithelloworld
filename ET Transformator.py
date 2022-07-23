476321a46ba1a8d3b03a135ba43713ae5072869d
jkomazec <60698944+jkomazec@users.noreply.github.com>
2022-07-23 19:49:30 +0200

    Add files via upload

---
+++ ET Transformator.py
@@ -0,0 +1,18 @@
+import streamlit as st
+import pandas as pd
+
+st.title('st.file_uploader')
+
+st.subheader('Input CSV')
+uploaded_file = st.file_uploader("Choose ET files", accept_multiple_files = True)
+
+if uploaded_file:
+  #df = pd.read_csv(uploaded_file)
+  et_raw = pd.concat([pd.read_csv(f, sep=';', keep_default_na=False) for f in uploaded_file])
+
+  st.subheader('DataFrame')
+  st.write(et_raw)
+  st.subheader('Descriptive Statistics')
+  st.write(et_raw.describe())
+else:
+  st.info('☝️ Upload a CSV file')
