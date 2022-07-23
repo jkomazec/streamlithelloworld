b7e25a91574f74bb52ee918346cba2c4236669ab
jkomazec <60698944+jkomazec@users.noreply.github.com>
2022-07-23 20:00:09 +0200

    Testing
    Testing if the raw change will work by commenting out row 11 and returning to default in the next row.

--- ET Transformator.py
+++ ET Transformator.py
@@ -8,7 +8,8 @@ uploaded_file = st.file_uploader("Choose ET files", accept_multiple_files = True
 
 if uploaded_file:
   #df = pd.read_csv(uploaded_file)
-  et_raw = pd.concat([pd.read_csv(f, sep=';', keep_default_na=False) for f in uploaded_file])
+  #et_raw = pd.concat([pd.read_csv(f, sep=';', keep_default_na=False) for f in uploaded_file])
+  et_raw = st.file_uploader("Choose a file")
 
   st.subheader('DataFrame')
   st.write(et_raw)
