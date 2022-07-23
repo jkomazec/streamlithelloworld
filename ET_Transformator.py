4a57775c2900e028d0289214e97f35ffa293e3bc
jkomazec <60698944+jkomazec@users.noreply.github.com>
2022-07-23 22:05:55 +0200

    Update ET_Transformator.py

--- ET_Transformator.py
+++ ET_Transformator.py
@@ -28,13 +28,17 @@ if uploaded_file:
 else:
   st.info('☝️ Upload a CSV file')
   
-def get_table_download_link(df):
-    """Generates a link allowing the data in a given panda dataframe to be downloaded
-    in:  dataframe
-    out: href string
-    """
-    csv = df.to_csv(index=False)
-    b64 = base64.b64encode(csv.encode()).decode()  # some strings <-> bytes conversions necessary here
-    href = f'<a href="data:file/csv;base64,{b64}">Download csv file</a>'
+@st.cache
+def convert_df(df):
+   return df.to_csv().encode('utf-8')
 
-st.markdown(get_table_download_link(et_raw), unsafe_allow_html=True)
+
+csv = convert_df(et_raw)
+
+st.download_button(
+   "Press to Download",
+   csv,
+   "file.csv",
+   "text/csv",
+   key='download-csv'
+)
