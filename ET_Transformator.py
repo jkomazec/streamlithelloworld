d42f2783b662989cb2f9989285f65ef49bd0cf08
jkomazec <60698944+jkomazec@users.noreply.github.com>
2022-07-23 22:03:50 +0200

    Update ET_Transformator.py

--- ET_Transformator.py
+++ ET_Transformator.py
@@ -37,4 +37,4 @@ def get_table_download_link(df):
     b64 = base64.b64encode(csv.encode()).decode()  # some strings <-> bytes conversions necessary here
     href = f'<a href="data:file/csv;base64,{b64}">Download csv file</a>'
 
-  st.markdown(get_table_download_link(et_raw), unsafe_allow_html=True)
+st.markdown(get_table_download_link(et_raw), unsafe_allow_html=True)
