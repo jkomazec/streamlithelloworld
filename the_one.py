a6ccb061e7fe120a106095929bd1c8d61ddcb664
jkomazec <60698944+jkomazec@users.noreply.github.com>
2022-08-18 16:06:37 +0200

    Update the_one.py

--- the_one.py
+++ the_one.py
@@ -1,17 +1,19 @@
-import savReaderWriter as spss
-import pandas as pd
-import json
-from PIL import Image, ImageDraw
 import streamlit as st 
+#import savReaderWriter as spss
+import pandas as pd
+#import json
+from PIL import Image, ImageDraw
+
 
 st.title('Click mapper')
 
 uploaded_db = st.file_uploader("Upload SPSS database")
-images = st.file_uploader("Upload JPGs", type="jpg")
+images = st.file_uploader("Upload JPGs", type="jpg", accept_multiple_files = True)
 
-if uploaded_db:
+if uploaded_db and images:
 	st.write("Brao")
-
+	for image in images:
+		image = Image.open(images)
 #	@st.cache
 
 else:
