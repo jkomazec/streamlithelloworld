cb1eb38405e8fda81271c06c996099f9592df9dd
jkomazec <60698944+jkomazec@users.noreply.github.com>
2022-08-18 16:08:27 +0200

    Update the_one.py

--- the_one.py
+++ the_one.py
@@ -1,12 +1,12 @@
 import streamlit as st 
 #import savReaderWriter as spss
-import pandas as pd
+#import pandas as pd
 #import json
-from PIL import Image, ImageDraw
+#from PIL import Image, ImageDraw
 
 
 st.title('Click mapper')
-
+'''
 uploaded_db = st.file_uploader("Upload SPSS database")
 images = st.file_uploader("Upload JPGs", type="jpg", accept_multiple_files = True)
 
@@ -18,4 +18,5 @@ if uploaded_db and images:
 
 else:
 	st.info('Upload a database')
+'''
 
